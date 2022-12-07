3ab5c22f9c2ad0e96549e977c66fbf997d1cf1b6
yizhangliu <yizhangliu@users.noreply.huggingface.co>
2022-12-07 09:42:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from pyChatGPT import ChatGPT
+import gradio as gr
 import os
 from loguru import logger
 
+session_token = os.environ.get('SessionToken')      
+logger.info(f"session_token_: {session_token}")
+
 def chat(text):
     try:
-      session_token = os.environ.get('SessionToken')      
-      logger.info(f"session_token_: {session_token}")
       api = ChatGPT(session_token) 
       resp = api.send_message(text)    
       api.refresh_auth() 
@@ -16,10 +18,33 @@ def chat(text):
       response = "Sorry, I'm am tired."
     return response
 
-import gradio as gr
-demo = gr.Interface(chat,
-              inputs = [gr.Textbox(label = 'Input： ')], 
-               outputs = gr.outputs.Textbox(type="text",label="from ChatGPT："), 
-               title = "talk with ChatGPT",
-               description= "")
+
+# demo = gr.Interface(chat,
+#               inputs = [gr.Textbox(label = 'Input： ')], 
+#                outputs = gr.outputs.Textbox(type="text",label="from ChatGPT："), 
+#                title = "Talk with ChatGPT",
+#                description= "")
+
+def chat(message, history):
+    history = history or []
+    message = message.lower()
+    if message.startswith("how many"):
+        response = random.randint(1, 10)
+    elif message.startswith("how"):
+        response = random.choice(["Great", "Good", "Okay", "Bad"])
+    elif message.startswith("where"):
+        response = random.choice(["Here", "There", "Somewhere"])
+    else:
+        response = "I don't know"
+    history.append((message, response))
+    return history, history
+
+chatbot = gr.Chatbot().style(color_map=("green", "pink"))
+demo = gr.Interface(
+    chat,
+    ["text", "state"],
+    [chatbot, "state"],
+    allow_flagging="never",
+)
+
 demo.launch(debug = True)
